048d939a4cd9cf1ec0733a6e02c49badc77a469a
Davide Benazzi <benazzi.davide@gmail.com>
2020-11-24 16:19:29 +0100

    Get albums from axios

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 const app = new Vue ({
     el: '#app',
     data: {
-        
+        albums: [],
+    },
+    created() {
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then ( response => { 
+            this.albums = response.data.response;
+            console.log(this.albums);
+        })
+        .catch( error => { 
+            console.log(error);
+        });
     },
 });
